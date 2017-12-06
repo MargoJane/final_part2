19542e591baff70ea6e2071ae143ffed567c50e0
MargoJane <31864917+MargoJane@users.noreply.github.com>
2017-12-06 16:03:44 -0500

    Add files via upload

---
+++ .gitignore/Assets/cubeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeScript : MonoBehaviour {
+	public int X;
+	public int Y;
+	public bool cubeActive = false;
+	gameController myGameController;
+
+	// Use this for initialization
+	void Start () {
+		myGameController = GameObject.Find ("GameObject").GetComponent<gameController> ();
+	}
+
+	void OnMouseDown(){
+		gameController.processClick (gameObject, X, Y, cubeActive);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
